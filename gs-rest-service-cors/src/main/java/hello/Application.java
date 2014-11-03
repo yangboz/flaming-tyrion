ee010389e97fd968acc31e62f4a788360bd5a035
yangboz <YoungWelle@gmail.com>
2014-11-03 14:19:06 +0800

    ADDED:gs-auth-ldap first part code base

--- gs-rest-service-cors/src/main/java/hello/Application.java
+++ gs-rest-service-cors/src/main/java/hello/Application.java
@@ -3,12 +3,24 @@ package hello;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
+//import org.activiti.engine.ProcessEngine;
+//import org.activiti.spring.boot.ProcessEngineAutoConfiguration;
+
+@Configuration
 @ComponentScan
 @EnableAutoConfiguration
+//@EnableAutoConfiguration(exclude = ProcessEngineAutoConfiguration.class)
+//@ImportResource("classpath:/hello/activiti.cfg.xml")
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    	ApplicationContext applicationContext = 
+    			SpringApplication.run(Application.class, args);
+//    	ProcessEngine processEngine = applicationContext.getBean(ProcessEngine.class);
+    	System.out.println("Init'd the app context with activiti process engine!");
     }
 }
